905201619c14ce16953b6b02831692c5ddb9cb5b
yigitkaan <65395094+yigitkaan@users.noreply.github.com>
2023-05-30 08:32:30 +0300

    Giriş sistemi eklendi

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horixontalThrow = Input.GetAxis("Horizontal");
+        Debug.Log(horixontalThrow);
+
+        float verticalThrow = Input.GetAxis("Vertical");
+        Debug.Log(verticalThrow);
+    }
+}
