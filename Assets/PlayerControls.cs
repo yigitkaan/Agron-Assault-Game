081be9bc72f30aac5808f97084bbadc16d2fc84a
yigitkaan <65395094+yigitkaan@users.noreply.github.com>
2023-05-31 08:56:48 +0300

    PlayerControl e yeni input system eklendi

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -1,22 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerControls : MonoBehaviour
-{
+public class PlayerControls : MonoBehaviour {
+    // NOT : Yeni input system i kullanabilmek için packet manager dan inputsystem i yükleyin.
+
+
+
+    [SerializeField] InputAction movement; //Yeni input system kullanımı için.
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        float horixontalThrow = Input.GetAxis("Horizontal");
-        Debug.Log(horixontalThrow);
+    //Bu metodları yapmamızın nedeni inputsystem i sadece ihtiyacıız olduğunda kullanabilmek içindir.
+    void OnEnable() {    // Yeni inputSystem i kullanmak için etkinleştirmemiz gerekir.
+        
+        movement.Enable();// Yeni inputsystem i etkinleştirir.
+    }
 
-        float verticalThrow = Input.GetAxis("Vertical");
+    void OnDisable() {
+
+        movement.Disable();
+    }
+    //----------------------------------------------------------------
+
+
+    // Update is called once per frame
+    void Update() {
+        ////Eski input system kullanımı
+        //float horizontalThrow = Input.GetAxis("Horizontal");
+        //float verticalThrow = Input.GetAxis("Vertical");
+
+
+        //Yeni input system kullanımı
+        float horizontalThrow = movement.ReadValue<Vector2>().x;
+        float verticalThrow = movement.ReadValue<Vector2>().y;
+
+        Debug.Log(horizontalThrow);
         Debug.Log(verticalThrow);
     }
 }
